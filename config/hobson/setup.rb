a719809eb3e8d3c6e1c310eca9921e7220ceda25
Jared Grippe and Tim James <tech_ops+jared+tjames@change.org>
2012-04-09 14:11:39 -0700

    hobson setup hook now creates untracked2.txt to help us test hobson's workspace reprepare functionality

--- config/hobson/setup.rb
+++ config/hobson/setup.rb
@@ -8,7 +8,7 @@ execute "which ruby"
 execute "ruby --version"
 
 # we create this file to test workspace.prepare optimizations
-File.open(File.expand_path('../../../untracked1.txt', __FILE__), 'w'){|file|
+File.open(File.expand_path('../../../untracked2.txt', __FILE__), 'w'){|file|
 file.write <<-TEXT
 this is an untracked file created during the workspace prepare phase.
 that should be stashed and restored when workspace.prepare uses
